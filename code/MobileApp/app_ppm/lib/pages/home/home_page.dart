50cc7454b4575500e8360cd2a8f8012954605107
Miguel Rosa <miguelrosa.programacao@gmail.com>
2021-05-17 15:49:02 +0100

    update connection

--- code/MobileApp/app_ppm/lib/pages/home/home_page.dart
+++ code/MobileApp/app_ppm/lib/pages/home/home_page.dart
@@ -7,7 +7,11 @@ import 'package:app_ppm/pages/components/ToastMessages.dart';
 
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key key}) : super(key: key);
+
+
+  HomePage({
+    Key key
+  }) : super(key: key);
 
 
   @override
@@ -17,7 +21,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   //_HomePageState();
-  //final Query _forCollection = FirebaseFirestore.instance.collection('tmp-percursos');
+  final Query _forCollection = FirebaseFirestore.instance.collection("tmp-percursos");//.orderBy("codRoute", "asc");
+
 
   @override
   Widget build(BuildContext context) {
@@ -25,14 +30,33 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         //TODO REVER UI
         title: Text("Percursos Pedestres"),
-        backgroundColor: Color.fromRGBO(0, 0, 0, 100),
-        foregroundColor: Colors.black,
-        elevation: 0,
+        backgroundColor: Colors.black,
       ),
 
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('tmp-percursos').snapshots(),
+        stream: _forCollection.orderBy("codRoute").snapshots(), //FirebaseFirestore.instance.collection('tmp-percursos').snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+
+          if(snapshot.hasError){
+            return Container(
+              child: Stack(
+                children: [
+                  BackgroundImage(),
+                  Text( "Ocorreu um erro. Por favor tente mais tarde." ,
+                      //textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Ubuntu',
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white,
+                        decoration: TextDecoration.none,
+                      )
+                  ),
+                ],
+              ),
+            );
+          }
+
           if( !snapshot.hasData ){
             return Center(
               child: CircularProgressIndicator(),
@@ -45,23 +69,52 @@ class _HomePageState extends State<HomePage> {
 
                   Center(
                     child: ListView(
+                      padding: EdgeInsets.fromLTRB(16, 32, 16, 0),
                       children: snapshot.data.docs.map((document) {
                         return Container(
                           width: MediaQuery.of(context).size.width / 1.2,
-                          child: Container(
-                            alignment: Alignment.centerLeft,
-                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                            height: MediaQuery.of(context).size.height / 6,
-                            child: Text( document['title'] ,
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontFamily: 'Ubuntu',
-                                  fontWeight: FontWeight.normal,
-                                  color: Colors.white,
-                                  decoration: TextDecoration.none,
-                                )
+                          height: MediaQuery.of(context).size.height / 8,
+                          child: GestureDetector(
+                            child: Card(
+                              elevation: 2,
+                              color: Colors.transparent,
+
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text( document['codRoute'] ,
+                                      //textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontFamily: 'Ubuntu',
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.white,
+                                        decoration: TextDecoration.none,
+                                      )
+                                  ),
+
+                                  Text( document['title'] ,
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: 'Ubuntu',
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                        decoration: TextDecoration.none,
+                                      )
+                                  ),
+
+                                  Icon(
+                                    Icons.arrow_forward_ios_rounded,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              ),
                             ),
+                            onTap: (){
+                              //TODO CREATE OPEN PAGE WITH DATA
+                              ToastMessages().showToast("Selected Route: \n \"" + document['title'].toString() + "\" ");
+                            },
                           ),
                         );
                       }).toList(),
